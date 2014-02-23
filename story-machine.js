78922260573cf11312544dd584e08f260d57d34d
James Holmes <32bitkid@gmail.com>
2014-02-23 19:19:28 +0000

    refactoring and removing internal dependency on mator

--- story-machine.js
+++ story-machine.js
@@ -1,105 +1,33 @@
-var storyMachine = (function() {
-  
-  var requestFrame = (function(){
-    return  window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame    ||
-      fallback;
-    
-    function fallback(callback) {
-        window.setTimeout(callback, 1000 / 60);
-    }
-  })();
-  
-  // Animator
-  var mator = (function() {
-    var requestFrame = (function() {
-      return  window.requestAnimationFrame       ||
-              window.webkitRequestAnimationFrame ||
-              window.mozRequestAnimationFrame    ||
-              fallback();
-      
-      function fallback() {
-        var start = +new Date()
-        return function fallbackAnimationFrame(callback) {
-          window.setTimeout(function() {
-            callback.call(this, (+new Date()) - start);
-          }, 1000 / 60);
-        };
-      }
-    })();
-    
-    // Animate
-    var fn = function(total, easing) {
-      easing = easing || fn.easing.linear;
-      
-      var updates = [];
-      var completed = false, start, worker = function(now) {
-        var ctx, percent, elasped, val,i,l;
-        
-        if(!start) start = now;
-        
-        elapsed = now - start;
-        if(elapsed > total) completed=true, elapsed=total;
-        percent = elapsed/total;
-        val = easing(percent, elapsed, 0, 1, total);
-        
-        for(i=0,l=updates.length;i<l;i++) updates[i](val);
-        
-        if(!completed) return requestFrame(worker);
-      }
-      
-      return ctx = {
-        start: function() { requestFrame(worker); return ctx; },
-        reset: function() { completed = false; start = undefined; return ctx; },
-        restart: function() { return ctx.reset().start(); },
-        update: function(fn, start, end, tr) {
-          var transformed = tr ? function(val) { fn(tr(val)); } : fn;
-          var result = (start != null) && (end != null) ?
-              function(val) { transformed(val*(end-start)+start); } : transformed;
-          updates.push(result);
-          return ctx;
-        }
-      };
-    };
-    
-    var toUnits = function(units) { return function(v) { return v+units; }; };
-    fn.pixels = toUnits("px");
-    fn.em = toUnits("em");
-    
-    var noop = function() {};
-    fn.with = function(ctx, target) {
-      if (target && target.call == Function.prototype.call) 
-        return function callback(val) { target.call(ctx, val); };
-      if(ctx && target)
-        return function(val) { ctx[target] = val; };
-      return noop;
-    };
-                 
-    // Easing functions.
-    fn.easing = (function() {
-      var quad = {
-        in: function (n,e,t,a,r) { return a*(e/=r)*e+t; },
-        out: function (n,e,t,a,r) { return-a*(e/=r)*(e-2)+t; },
-        both: function (n,e,t,a,r) { return(e/=r/2)<1?a/2*e*e+t:-a/2*(--e*(e-2)-1)+t; }
-      };
-                 
-      return {
-        "linear": function(n,e,t,a,r) { return n*(a-t)+t; },
-        "quad": quad
-      };
-    }());
-    
-    return fn;
-  }());  
+(function() {
   
   var bind = Function.prototype.bind,
       call = Function.prototype.call,
-      slice = call.bind(Array.prototype.slice);
-  
-  return function storyMachine(options) {
+      slice = call.bind(Array.prototype.slice),
+      extend = function(obj) {
+        var rest = slice(arguments, 1), i, l, source, prop;
+        for(i=0,l=rest.length;i<l;i++) {
+          source = rest[i];
+          for(prop in source) {
+            obj[prop] = source[prop];
+          }
+        }
+        return obj;
+      },
+      defaultOptions = {
+        target: undefined,
+        elements: undefined,
+        onSectionAdded: function(next) {
+          var root = document.documentElement || document.body;
+          root.scrollTop = next.offsetTop;
+        },
+        register: {}
+      };
+
+  var storyMachine = function(options) {
+    options = extend({}, defaultOptions, options);
+    
     var handleClick = function(e) {
-      var elem = e.target, node = e.currentTarget;
+      var elem = e.target, node = e.currentTarget, answer;
       
       if(node.classList.contains("complete")) {
         e.preventDefault();
@@ -109,55 +37,56 @@ var storyMachine = (function() {
       // Find closest `.answer`
       do {
         if(elem.classList.contains("answer")) {
-          var answer = elem;
-          
-          e.preventDefault();
-          var name = answer.getAttribute("href").substr(1)
-          if(!has(name)) return;
-          
-          var next = render(name);
-          next.classList.add("current");
-          next.classList.add("new"); 
-          // force layout
-          var top = next.offsetTop; 
-          next.classList.remove("new");
-          
-          answer.classList.add("selected");
-          
-          node.classList.add("complete");
-          node.classList.remove("current");
-          
-          var root = document.documentElement || document.body;
-          
-          top = Math.min(top,root.offsetHeight- root.clientHeight)
-
-          mator(options.speed || 750, mator.easing.quad.both) 
-            .update(mator.with(root, "scrollTop"), root.scrollTop, top)
-            .start();
-          return;
+          answer = elem;
+          break;
         }
-      } while(elem=elem.parentElement);
+      } while((elem=elem.parentElement) !== null);
+
+      if(answer===undefined) return;
+
+      e.preventDefault();
+
+      var name = answer.getAttribute("href").substr(1);
+
+      if(!has(name)) return;
+
+      var next = render(name);
+      next.classList.add("current");
+      next.classList.add("new");
+      var forceLayout = next.offsetTop; // force layout
+      next.classList.remove("new");
+
+      answer.classList.add("selected");
+
+      node.classList.add("complete");
+      node.classList.remove("current");
+
+      var fn = options.onSectionAdded;
+      if(fn && fn.call == call) fn.call(void 0, next);
+
+      return;
     };
     
     
-    var target = options.target;
-    var cache = {};
-    var typeHandlers = {};    
-    var tagHandlers = {
-      "DIV": function(element) {
-        var name = element.getAttribute("id"),
-            content = element.innerHTML;
-        add(name, content);
-      },
-      "SCRIPT": function(element) {
-        var name = element.getAttribute("id"),
-            content = (element.innerText || element.textContent),
-            handler = typeHandlers[element.getAttribute("type")];
+    var target = options.target,
+        cache = {},
+        typeHandlers = {},
+        tagHandlers = {
+          "DIV": function handleDiv(element) {
+            var name = element.getAttribute("id"),
+                content = element.innerHTML;
+            add(name, content);
+          },
+          "SCRIPT": function handleScript(element) {
+            var name = element.getAttribute("id"),
+                content = (element.innerText || element.textContent),
+                handler = typeHandlers[element.getAttribute("type")];
 
-        if(handler) content = handler(content);
-        add(name, content);
-      }
-    }
+            if(handler) content = handler(content);
+            add(name, content);
+          }
+        },
+        defaultTagHandler = tagHandlers.DIV;
 
     var has = function(name) { return !!cache[name]; },
         add = function(name, content) { cache[name] = content; },
@@ -176,22 +105,31 @@ var storyMachine = (function() {
         addElements = function(nodeList) {
           var i,l,element, handler;
            
-          for(var i=0,l=nodeList.length;i<l;i++) {
+          for(i=0,l=nodeList.length;i<l;i++) {
             element = nodeList[i];
             element.parentElement.removeChild(element);
-            handler = tagHandlers[element.tagName] || tagHandlers["DIV"];
+            handler = tagHandlers[element.tagName] || defaultTagHandler;
             handler(element);
           }
         };
     
-    if(options && options.register) {
+    if(options.register) {
       for(var key in options.register) {
         registerType(key, options.register[key]);
       }
     }
     
-    if(options && options.elements) addElements(options.elements);
+    if(options.elements) addElements(options.elements);
     
     return { add: add, render: render, addElements: addElements, registerType: registerType };
   };
-}());
+
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = storyMachine;
+    }
+    exports.storyMachine = storyMachine;
+  } else {
+    this.storyMachine = storyMachine;
+  }
+}).call(this);
